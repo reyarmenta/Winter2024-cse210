8256dd0f9cd0f08f3fcade025c25fe39f38c2f37
reyarmenta <reyarmenta1@hotmail.com>
2024-01-13 23:25:00 -0700

    W01 Prove Developer

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,21 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
     }
-}
+
+    List<int> numbers = new List<int>();
+    int input = 8;
+    
+    while (input != 0)
+    {
+        Console.WriteLine("Please enter a number:");
+        input = Console.ReadLine();
+        numbers.Add(input);
+    }
+
+}   
