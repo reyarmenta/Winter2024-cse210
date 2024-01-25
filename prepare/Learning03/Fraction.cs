59943ea584e491230440845e95bd4d2e19911efe
reyarmenta <reyarmenta1@hotmail.com>
2024-01-24 21:18:36 -0600

    Encapsulation Learning Activity

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,35 @@
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+    
+    public Fraction(int Number)
+    {
+        _top = Number;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public string GetFractionString()
+    {
+        string text = $"{_top}/{_bottom}";
+        return text;
+    }
+
+    public double GedDecimalValue()
+    {
+        return (double)_top / (double)_bottom;
+    }
+
+}
